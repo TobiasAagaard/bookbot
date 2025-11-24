9431d3a8cdccdc302ec2da6fedf57687609b0c6b
TobiasAagaard <tach@trifork.com>
2025-11-24 12:39:36 +0100

    print count_unique_characters

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from stats import get_book_text, total_num_words 
+from stats import get_book_text, total_num_words , count_unique_characters
 
 
 def main():
     print(f"Found {total_num_words(get_book_text('books/frankenstein.txt'))} total words")
+    print(str(count_unique_characters(get_book_text('books/frankenstein.txt'))) + " unique characters")
 
 
 main()
--- stats.py
+++ stats.py
@@ -8,3 +8,10 @@ def total_num_words(filecontent):
     words = filecontent.split()
     return len(words)
 
+def count_unique_characters(filecontent):
+    text = filecontent.lower()
+    character_count = {}
+    for char in text:
+        if char.isalpha():
+            character_count[char] = character_count.get(char, 0) + 1
+    return character_count
